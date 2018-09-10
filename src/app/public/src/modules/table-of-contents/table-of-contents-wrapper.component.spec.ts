2c096c3f868d813fb04ecd177723a44fb5595ce6
Blackbaud-TimPepper <tim.pepper@blackbaud.com>
2018-09-10 14:22:18 -0400

    fix table of contents tests and merge with master

--- src/app/public/src/modules/table-of-contents/table-of-contents-wrapper.component.spec.ts
+++ src/app/public/src/modules/table-of-contents/table-of-contents-wrapper.component.spec.ts
@@ -1,14 +1,13 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { Pipe, PipeTransform } from '@angular/core';
-import { expect } from '@blackbaud/skyux-lib-testing';
-import { StacheNavComponent, StacheNavService, StacheNavLink } from '../nav';
-import { Subject } from 'rxjs';
-import { StacheLinkModule } from '../link';
-import { SkyAppResourcesService } from '@blackbaud/skyux-builder/runtime/i18n';
-import { SkyMediaQueryModule } from '@blackbaud/skyux/dist/core';
 import { StacheTableOfContentsWrapperComponent } from './table-of-contents-wrapper.component';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { PipeTransform, Pipe, Renderer2 } from '@angular/core';
+import { SkyModule } from '@blackbaud/skyux/dist/core';
+import { expect } from '@blackbaud/skyux-lib-testing';
+import { SkyAppResourcesService } from '@blackbaud/skyux-builder/runtime/i18n';
+import { StacheWindowRef, StacheRouteService } from '../shared';
 import { StacheTableOfContentsComponent } from './table-of-contents.component';
-import { StacheWindowRef } from '../shared';
+import { StacheNavModule, StacheNavLink } from '../nav';
+import { RouterTestingModule } from '@angular/router/testing';
 
 @Pipe({
   name: 'skyAppResources'
@@ -17,6 +16,22 @@ export class MockSkyAppResourcesPipe implements PipeTransform {
   public transform(value: number): number {
     return value;
   }
+}
+
+class MockWindowService {
+  public nativeWindow = {
+    document: {
+      documentElement: {
+        querySelector: () => {}
+      },
+      body: {
+        classList: {
+          add: () => {}
+        }
+      }
+    },
+    innerWidth: 100
+  };
 }
 
 class MockSkyAppResourcesService {
@@ -36,87 +51,66 @@ class MockSkyAppResourcesService {
   }
 }
 
-describe('StacheTableOfContentsWrapperComponent', () => {
+class MockStacheRouteService {
+  public getActiveUrl = () => '';
+}
+
+class MockRenderer {
+  public classList: any[] = [];
+  public addClass = (el: any, classname: any) => {};
+  public removeClass = (el: any, classname: any) => {};
+}
+
+const route: StacheNavLink = {
+  name: 'string',
+  path: '/string',
+  offsetTop: 123,
+  isActiveTocAnchor: false
+};
+
+describe('Table of Contents Wrapper Component', () => {
   let component: StacheTableOfContentsWrapperComponent;
   let fixture: ComponentFixture<StacheTableOfContentsWrapperComponent>;
-  let mockElement: Element = document.createElement('body');
-  let mockWindowRef: any;
+
   let mockSkyAppResourcesService: any;
-
-  class MockWindowService {
-    public nativeWindow = {
-      document: {
-        body: mockElement,
-        documentElement: {
-          getBoundingClientRect: () => {
-            return { bottom: 100 };
-          },
-          querySelector: jasmine.createSpy('querySelector').and.callFake(function(selector: string) {
-            return {
-              textContent: 'test',
-              classList: {
-                add(cssClass: string) { }
-              },
-              scrollIntoView() { },
-              offsetHeight: 50,
-              getBoundingClientRect() {
-                return {
-                  top: 100
-                };
-              }
-            };
-          })
-        }
-      },
-      innerHeight: 100,
-      pageYOffset: 400
-    };
-
-    public testElement = {
-      offsetTop: 100
-    };
-
-    public onResize$ = new Subject();
-  }
-
-  const route: StacheNavLink = {
-    name: 'string',
-    path: '/string',
-    offsetTop: 123,
-    isActiveTocAnchor: false
-  };
+  let mockWindowService: any;
+  let mockWindowRef: any;
+  let mockStacheRouteService: any;
+  let mockRenderer: any;
 
   beforeEach(() => {
-    mockWindowRef = new MockWindowService();
+    mockWindowService = new MockWindowService();
     mockSkyAppResourcesService = new MockSkyAppResourcesService();
+    mockWindowRef = new MockWindowService();
+    mockStacheRouteService = new MockStacheRouteService();
+    mockRenderer = new MockRenderer();
 
     TestBed.configureTestingModule({
+      imports: [
+        SkyModule,
+        StacheNavModule,
+        RouterTestingModule
+      ],
       declarations: [
-        StacheNavComponent,
         StacheTableOfContentsComponent,
         StacheTableOfContentsWrapperComponent,
         MockSkyAppResourcesPipe
       ],
-      imports: [
-        StacheLinkModule,
-        SkyMediaQueryModule
-      ],
       providers: [
-        StacheNavService,
+        { provide: StacheRouteService, useValue: mockStacheRouteService },
         { provide: SkyAppResourcesService, useValue: mockSkyAppResourcesService },
-        { provide: StacheWindowRef, useValue: mockWindowRef }
+        { provide: StacheWindowRef, useValue: mockWindowService },
+        { provide: Renderer2, useValue: mockRenderer }
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(StacheTableOfContentsWrapperComponent);
-    fixture.detectChanges();
     component = fixture.componentInstance;
-    component.tocRoutes = [ route ];
-    component.ngAfterViewInit();
+
+    component.tocRoutes = [route];
   });
 
-  it('should render the component', () => {
+  it('should exist', () => {
     expect(fixture).toExist();
   });
 
@@ -128,16 +122,8 @@ describe('StacheTableOfContentsWrapperComponent', () => {
     expect(component.tocOpen).toEqual(false);
   });
 
-  it('should call the check the window width on window resize', () => {
-    component.tocOpen = false;
-    mockWindowRef.nativeWindow.innerWidth = 10;
-    mockWindowRef.onResize$.next();
+  it('should pass accessibility', async(() => {
     fixture.detectChanges();
-    expect(component.tocOpen).toBe(false);
-  });
-
-  it('should be accessible', async(() => {
-    fixture.detectChanges();
-    expect(fixture.debugElement.nativeElement).toBeAccessible();
+    expect(fixture.nativeElement).toBeAccessible();
   }));
 });
